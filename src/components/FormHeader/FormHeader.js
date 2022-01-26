4d60071704b967b7db06e229d64e2c4249ce27b4
Emerson <eemr3@yahoo.com.br>
2022-01-25 21:23:19 -0300

    fix: corrigi erros de lint de novo

--- src/components/FormHeader/FormHeader.js
+++ src/components/FormHeader/FormHeader.js
@@ -14,10 +14,13 @@ function FormHeader() {
           </Form.Select>
         </Col>
         <Col>
-          <Form.Select aria-label="Default select example">
-            <option value="1">One</option>
-            <option value="2">Two</option>
-            <option value="3">Three</option>
+          <Form.Select
+            aria-label="Default select example"
+            data-testid="comparison-filter"
+          >
+            <option value="maoir">Maior que</option>
+            <option value="menor">Menor que</option>
+            <option value="igual">Igual</option>
           </Form.Select>
         </Col>
         <Col>
